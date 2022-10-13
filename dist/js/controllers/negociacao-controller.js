d42e77a6ca4799468c9497e8709a0d1876a9c845
Matheus <matheus@pop-os.localdomain>
2022-10-13 15:56:47 -0300

    primeiro commit

---
+++ dist/js/controllers/negociacao-controller.js
@@ -0,0 +1,30 @@
+import { Negociacao } from "../models/negociacao.js";
+import { Negociacoes } from "../models/negociacoes.js";
+export class NegociacaoController {
+    constructor() {
+        this.negoaciacoes = new Negociacoes();
+        this.inputData = document.querySelector("#data");
+        this.inputQuantidade = document.querySelector("#quantidade");
+        this.inputValor = document.querySelector("#valor");
+    }
+    adiciona() {
+        const negociacao = this.criaNegociacao();
+        this.negoaciacoes.adiciona(negociacao);
+        this.negoaciacoes.lista();
+        console.log(this.negoaciacoes.lista());
+        this.limparFormulario();
+    }
+    criaNegociacao() {
+        const exp = /-/g;
+        const data = new Date(this.inputData.value.replace(exp, ","));
+        const quantidade = parseInt(this.inputQuantidade.value);
+        const valor = parseInt(this.inputValor.value);
+        return new Negociacao(data, quantidade, valor);
+    }
+    limparFormulario() {
+        this.inputData.value = '';
+        this.inputQuantidade.value = '';
+        this.inputValor.value = '';
+        this.inputData.focus();
+    }
+}
